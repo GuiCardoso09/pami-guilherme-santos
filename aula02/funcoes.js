3b755614cd57b1debe04486ed47dba720eebb926
Guilherme <gui.cardosofaz10@gmail.com>
2026-09-10 21:56:23 -0300

    Aula sobre funções

---
+++ aula02/funcoes.js
@@ -0,0 +1,31 @@
+// function MultiplicaNumeros() {
+//     let n1 = 2;
+//     let n2 = 3;
+
+//     console.log(n1*n2);
+//     // ^ (potência) % (sobra de divisão [o que sobra de uma divisão exata mais próxima
+//     // exemplo: 5/2 = 1])
+// }
+// //MultiplicaNumeros();
+
+// function MultiplicacomParametros(v1, v2) {
+//     console.log(v1, "x", v2, "=", v1*v2);
+// }
+
+// MultiplicacomParametros(3,4);
+// MultiplicacomParametros(4,10);
+// MultiplicacomParametros(6,7)
+
+// function EquacaoDeBurrinho(x, y, z){
+//     let resultado = (x+y)/z;
+//     return resultado //calcule a equação e me dê a
+//   //  console.log("(x", "+","y)", "/", "z", "=", resultado)
+// }
+
+// console.log(EquacaoDeBurrinho(x=2,y=3,z=5));
+
+const EquacaoDeTontinho = (x,y,z,w) => {
+    return resultado = (x+y)*(z+w)/2; 
+}
+
+console.log("O resultado é: ", EquacaoDeTontinho(3,5,8,13));
